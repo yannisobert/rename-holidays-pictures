921d00c98d6ffe157fcb9b5aa0d114eb7cd2432a
yannisobert <90550556+yannisobert@users.noreply.github.com>
2022-07-17 14:44:45 +0200

    feat/error-management

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
 
+// errors
+if ($argv[2] === null){
+    print_r('Argument missing.');
+
+    exit(1);
+} elseif (count($argv) > 3) {
+    print_r('Too many arguments.');
+
+    exit(1);
+}
+
+// for files
 $dir = file_get_contents(__DIR__ . '/base');
 $files = glob('base/*.*');
 
+// arguments
 $city = $argv[1];
 $startNumber = $argv[2];
 
@@ -10,10 +23,11 @@ mkdir($city);
 
 foreach($files as $file) {
     $information = exif_read_data($file, 0, true);
-
     $date = $information['IFD0']['DateTime'];
 
     rename($file, $city . '/' . $city . '_' . $startNumber . '_' . $date . '.NEF');
 
     $startNumber++;
 }
+
+exit(0);
